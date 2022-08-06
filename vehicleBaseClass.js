413608fea364d477735e5e8c2f9d68896e7f072f
Connor <connor5741213@gmail.com>
2022-08-06 14:07:28 -0400

    finished commit

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,18 +1,42 @@
 class Vehicle {
-    constructor(model, brand, color, year, user) {
+    constructor(model, brand, color, year, mileage, user) {
     this.model = model
     this.brand = brand
     this.color = color
     this.year = year
+    this.mileage = mileage
     this.user = user
     }
-    make() {            //method
-        return "My favorite vehicle is the " + this.year + ", " + this.brand + ", " + this.model;
-    }                 
+            //methods     // enter your car information to see how many miles your car has on it
+    info() {            
+        return "Your vehicle information: " + this.year + ", " + this.brand + ", " + this.model;
+    }   
+
+    miles() {
+        if (this.mileage > 500) {
+            console.log("Your car needs a oil change")
+        }
+        else {
+            console.log("Your all good")
+        }
+    }  
+
+    coupon() {
+        if (this.brand === "Honda") {
+            console.log("You recieve a 20% discount on your next Honda purchase")
+        }
+        else {
+            console.log("Honda hates you forever")
+        }
+    }
 }
 
 
 
-let porche = new Vehicle("718 Cayman","Porche","red","2005","Connor")
-let vehicleYear = porche.make('2005');
-console.log(vehicleYear)
+let yourVehicle = new Vehicle("718 Cayman","Honda","red","2005",200,"Connor")
+let vehicleInfo = yourVehicle.info();
+console.log(vehicleInfo)
+
+let vehicleMileage = yourVehicle.miles();
+let vehicleCoupon = yourVehicle.coupon()
+
